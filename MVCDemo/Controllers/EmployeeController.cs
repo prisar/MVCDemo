b745f8c20aafd7de3ec5ba4bf4d6f0d26f505a80
Pritimay Sarkar <pritimaysarkar2010@gmail.com>
2018-01-23 18:33:03 +0530

    Models in mvc

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 101,
+                Name = "John",
+                Gender = "Male",
+                City = "London"
+            };
+
+            return View(employee);
+        }
+    }
+}
